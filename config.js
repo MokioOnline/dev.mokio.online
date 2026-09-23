6ddf9dcde6ddacf5679a672f5bf2c1736ebca421
MokioOnline <mokiobusiness@gmail.com>
2026-09-23 14:34:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.MOKIO_CONFIG = {
-  USER_URL: "https://YOUR-USERS-PROJECT.supabase.co",
-  USER_KEY: "paste_users_anon_eyJ_key_here",
-  MOKIO_URL: "https://YOUR-MESSAGING-PROJECT.supabase.co",
-  MOKIO_KEY: "paste_messaging_anon_eyJ_key_here",
+  USER_URL: "https://gmncuelonmicdbpuacqi.supabase.co",
+  USER_KEY: "sb_publishable_u09NHV7z9E-2CJ0tvQ8IvQ_xcSXfs0F",
+  MOKIO_URL: "https://kxqherkdmnqfscbzsoxq.supabase.co",
+  MOKIO_KEY: "sb_publishable_Kh8F9sYevLtVDVmRtCQFWg_dst_0oD9",
 };
