41ea467e1799b97c2994f1e75d8f961827c42e23
MokioOnline <mokiobusiness@gmail.com>
2026-09-23 14:27:30 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.MOKIO_CONFIG = {
+  USER_URL: "https://YOUR-USERS-PROJECT.supabase.co",
+  USER_KEY: "paste_users_anon_eyJ_key_here",
+  MOKIO_URL: "https://YOUR-MESSAGING-PROJECT.supabase.co",
+  MOKIO_KEY: "paste_messaging_anon_eyJ_key_here",
+};
