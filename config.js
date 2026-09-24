c2e12ce994304cc5fd8b4f64af3bc5406f5db8ad
MokioOnline <mokiobusiness@gmail.com>
2026-09-24 18:48:34 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.MOKIO_CONFIG = {
-  USER_URL: "https://gmncuelonmicdbpuacqi.supabase.co",
-  USER_KEY: "sb_publishable_u09NHV7z9E-2CJ0tvQ8IvQ_xcSXfs0F",
-  MOKIO_URL: "https://kxqherkdmnqfscbzsoxq.supabase.co",
-  MOKIO_KEY: "sb_publishable_Kh8F9sYevLtVDVmRtCQFWg_dst_0oD9",
-};
